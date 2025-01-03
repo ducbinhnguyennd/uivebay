a624c3e76114669c110e096937af11a6d1586003
trafdual <traz08102003@gmail.com>
2025-01-03 19:35:16 +0700

    first commit

--- src/Layout/SearchLayout/SearchLayout.js
+++ src/Layout/SearchLayout/SearchLayout.js
@@ -1,14 +1,19 @@
-import { useState,useEffect } from "react";
-import "./SearchLayout.scss";
-import { useToast } from "../../components/useToast/ToastContext";
-import FilterComponent from "../../components/SideBar/Filter";
-import SearchSidebar from "../../components/SideBar/SearchSideBar";
-function SearchLayout() {
-  const { cityfrom, cityto, searchData } = useToast();
-  console.log(searchData);
-  const [activeDate, setActiveDate] = useState("Thứ Bảy");
-  const [visibleDetailIndex, setVisibleDetailIndex] = useState(null);
+import { useState, useEffect } from 'react'
+import './SearchLayout.scss'
+import { useToast } from '../../components/useToast/ToastContext'
+import FilterComponent from '../../components/SideBar/Filter'
+import SearchSidebar from '../../components/SideBar/SearchSideBar'
+function SearchLayout () {
+  const { cityfrom, cityto, searchData } = useToast()
+  console.log(searchData)
+  const [activeDate, setActiveDate] = useState('Thứ Bảy')
+  const [visibleDetailIndex, setVisibleDetailIndex] = useState(null)
   const [hangmaybay, sethangmaybay] = useState([])
+  const [filters, setFilters] = useState({
+    sortBy: 'airline',
+    airlines: []
+  })
+
   const fetchhang = async () => {
     try {
       const response = await fetch('http://localhost:8080/gethangmaybay')
@@ -25,143 +30,206 @@ function SearchLayout() {
     fetchhang()
   }, [])
   const dates = [
-    { day: "Thứ Hai", date: "06/01", price: "868,000đ" },
-    { day: "Thứ Ba", date: "06/01", price: "868,000đ" },
-    { day: "Thứ Tư", date: "01/01", price: "" },
-    { day: "Thứ Năm", date: "02/01", price: "" },
-    { day: "Thứ Sáu", date: "03/01", price: "1,010,000đ" },
-    { day: "Thứ Bảy", date: "04/01", price: "868,000đ" },
-    { day: "Chủ Nhật", date: "05/01", price: "1,008,000đ" },
-  ];
+    { day: 'Thứ Hai', date: '06/01', price: '868,000đ' },
+    { day: 'Thứ Ba', date: '06/01', price: '868,000đ' },
+    { day: 'Thứ Tư', date: '01/01', price: '' },
+    { day: 'Thứ Năm', date: '02/01', price: '' },
+    { day: 'Thứ Sáu', date: '03/01', price: '1,010,000đ' },
+    { day: 'Thứ Bảy', date: '04/01', price: '868,000đ' },
+    { day: 'Chủ Nhật', date: '05/01', price: '1,008,000đ' }
+  ]
 
   const flights = [
     {
-      code: "VU750",
-      time: "05:45 - 07:55",
-      price: "868,000đ",
+      code: 'VU750',
+      time: '05:45 - 07:55',
+      price: '868,000đ',
       details: [
-        "Thời gian bay: 2 giờ 10 phút",
-        "Hãng hàng không: Vietravel Airlines",
-        "Loại vé: Economy",
-        "Số ghế: 12A",
-      ],
+        'Thời gian bay: 2 giờ 10 phút',
+        'Hãng hàng không: Vietravel Airlines',
+        'Loại vé: Economy',
+        'Số ghế: 12A'
+      ]
     },
     {
-      code: "VU794",
-      time: "20:45 - 22:55",
-      price: "868,000đ",
+      code: 'VU794',
+      time: '20:45 - 22:55',
+      price: '868,000đ',
       details: [
-        "Thời gian bay: 2 giờ 10 phút",
-        "Hãng hàng không: Vietravel Airlines",
-        "Loại vé: Economy",
-        "Số ghế: 15C",
-      ],
-    },
-  ];
+        'Thời gian bay: 2 giờ 10 phút',
+        'Hãng hàng không: Vietravel Airlines',
+        'Loại vé: Economy',
+        'Số ghế: 15C'
+      ]
+    }
+  ]
 
-  const handleDateClick = (day) => {
-    setActiveDate(day);
-  };
+  const handleDateClick = day => {
+    setActiveDate(day)
+  }
 
-  const toggleDetails = (index) => {
-    setVisibleDetailIndex(visibleDetailIndex === index ? null : index);
-  };
+  const toggleDetails = index => {
+    setVisibleDetailIndex(visibleDetailIndex === index ? null : index)
+  }
   const getAirlineImage = airlineCode => {
     const airline = hangmaybay.find(h => h.mahangmaybay === airlineCode)
     return airline ? airline.image : ''
   }
-  
+
+  const applyFilters = flights => {
+    let filteredFlights = [...flights]
+
+    if (filters.airlines.length > 0) {
+      filteredFlights = filteredFlights.filter(flight =>
+        filters.airlines.includes(flight.airlineCode)
+      )
+    }
+
+    if (filters.sortBy === 'price') {
+      filteredFlights.sort((a, b) => {
+        const priceA = parseInt(a.price.replace(/,/g, ''), 10)
+        const priceB = parseInt(b.price.replace(/,/g, ''), 10)
+        return priceA - priceB
+      })
+    } else if (filters.sortBy === 'time') {
+      filteredFlights.sort((a, b) => {
+        const timeToDate = time => {
+          const [hours, minutes] = time.split(':').map(Number)
+          const date = new Date()
+          date.setHours(hours, minutes, 0, 0)
+          return date
+        }
+
+        return timeToDate(a.departureTime) - timeToDate(b.departureTime)
+      })
+    } else if (filters.sortBy === 'airline') {
+      filteredFlights.sort((a, b) => {
+        const airlineA = a.airlineCode.toLowerCase()
+        const airlineB = b.airlineCode.toLowerCase()
+        if (airlineA < airlineB) return -1
+        if (airlineA > airlineB) return 1
+        return 0
+      })
+    }
+
+    return filteredFlights
+  }
+
+  const handleFiltersChange = newFilters => {
+    setFilters(newFilters)
+  }
+
+  const flights1 = applyFilters(searchData.outBound.data.flights)
+
   return (
-    <div className="search-layout">
-      <div className="content-wrapper">
-        {/* Phần bên trái */}
-        <div className="main-content">
-        <div className='flight-booking'>
-      <div className='booking-header'>
-        <div className='route-info'>
-          <span className='city'>{cityfrom}</span>&nbsp;
-          <img src='/plane1.png' alt='plane' style={{ width: '15px' }} />
-          &nbsp;
-          <span className='city'>{cityto}</span>
-          <br />
-          <div className='date-info'>
-            <span className='selected-date'>Thứ Bảy 04/01/2025</span>, tức ngày
-            5 âm lịch
-          </div>
-        </div>
-        <div className='price-info'>
-          Giá vé chưa gồm thuế và phí
-          <br />
-          <span className='note'>
-            <img src='./hanhly.png' alt='baggage' style={{ width: '18px' }} />
-            <img src='./suatan.jpg' alt='meal' style={{ width: '18px' }} />
-            giá vé đã bao gồm hành lý và suất ăn
-          </span>
-        </div>
-      </div>
-
-      <div className='date-selection'>
-        {dates.map(({ day, date, price }) => (
-          <div
-            key={day}
-            className={`date ${activeDate === day ? 'active' : ''}`}
-            onClick={() => handleDateClick(day)}
-          >
-            {day}
-            <br />
-            {price || date}
-          </div>
-        ))}
-      </div>
-
-      <div className='flight-options'>
-        {(activeDate === 'Thứ Bảy' || activeDate === 'Thứ Hai') &&
-          Array.isArray(searchData.outBound.data.flights) &&
-          searchData.outBound.data.flights.map((flight, index) => (
-            <div key={index}>
-              <div className='flight-row'>
-                <div className='flight-info'>
-                    <span>
-                        <img src={getAirlineImage(flight.airlineCode)} alt="" />
-                    </span>
-                  <span className='flight-code'>{flight.flightNumber}</span>
-                  <span className='flight-time'>
-                    {flight.departureTime} - {flight.arrivalTime}
-                  </span>
+    <div className='search-layout'>
+      <div className='content-wrapper'>
+        <div className='main-content'>
+          <div className='flight-booking'>
+            <div className='booking-header'>
+              <div className='route-info'>
+                <span className='city'>{cityfrom}</span>&nbsp;
+                <img src='/plane1.png' alt='plane' style={{ width: '15px' }} />
+                &nbsp;
+                <span className='city'>{cityto}</span>
+                <br />
+                <div className='date-info'>
+                  <span className='selected-date'>Thứ Bảy 04/01/2025</span>, tức
+                  ngày 5 âm lịch
                 </div>
-                <div className='flight-price'>{flight.price}</div>
-                <button
-                  className='flight-details'
-                  onClick={() => toggleDetails(index)}
-                >
-                  {visibleDetailIndex === index ? 'Ẩn chi tiết' : 'Chi tiết'}
-                </button>
-                <button className='select-flight'>{flight.chooseText}</button>
               </div>
-              {visibleDetailIndex === index && (
-                <div className='flight-detail-content'>
-                  <p>Chi tiết chuyến bay:</p>
-                  <ul>
-                    {flights.map((detail, i) => (
-                      <li key={i}>{detail}</li>
-                    ))}
-                  </ul>
-                </div>
-              )}
+              <div className='price-info'>
+                Giá vé chưa gồm thuế và phí
+                <br />
+                <span className='note'>
+                  <img
+                    src='./hanhly.png'
+                    alt='baggage'
+                    style={{ width: '18px' }}
+                  />
+                  <img
+                    src='./suatan.jpg'
+                    alt='meal'
+                    style={{ width: '18px' }}
+                  />
+                  giá vé đã bao gồm hành lý và suất ăn
+                </span>
+              </div>
             </div>
-          ))}
-      </div>
-    </div>
+
+            <div className='date-selection'>
+              {dates.map(({ day, date, price }) => (
+                <div
+                  key={day}
+                  className={`date ${activeDate === day ? 'active' : ''}`}
+                  onClick={() => handleDateClick(day)}
+                >
+                  {day}
+                  <br />
+                  {price || date}
+                </div>
+              ))}
+            </div>
+
+            <div className='flight-options'>
+              {(activeDate === 'Thứ Bảy' || activeDate === 'Thứ Hai') &&
+                Array.isArray(searchData.outBound.data.flights) &&
+                flights1.map((flight, index) => (
+                  <div key={index}>
+                    <div className='flight-row'>
+                      <div className='flight-info'>
+                        <span>
+                          <img
+                            src={getAirlineImage(flight.airlineCode)}
+                            alt=''
+                          />
+                        </span>
+                        <span className='flight-code'>
+                          {flight.flightNumber}
+                        </span>
+                        <span className='flight-time'>
+                          {flight.departureTime} - {flight.arrivalTime}
+                        </span>
+                      </div>
+                      <div className='flight-price'>{flight.price}</div>
+                      <button
+                        className='flight-details'
+                        onClick={() => toggleDetails(index)}
+                      >
+                        {visibleDetailIndex === index
+                          ? 'Ẩn chi tiết'
+                          : 'Chi tiết'}
+                      </button>
+                      <button className='select-flight'>
+                        {flight.chooseText}
+                      </button>
+                    </div>
+                    {visibleDetailIndex === index && (
+                      <div className='flight-detail-content'>
+                        <p>Chi tiết chuyến bay:</p>
+                        <ul>
+                          {flights.map((detail, i) => (
+                            <li key={i}>{detail}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                  </div>
+                ))}
+            </div>
+          </div>
         </div>
 
-        {/* Phần bên phải */}
-        <div className="filter-sidebar">
-          <FilterComponent />
-          <SearchSidebar/>
+        <div className='filter-sidebar'>
+          <FilterComponent
+            filters={filters}
+            onFiltersChange={handleFiltersChange}
+          />
+          <SearchSidebar />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default SearchLayout;
+export default SearchLayout
--- src/components/SideBar/Filter.js
+++ src/components/SideBar/Filter.js
@@ -1,138 +1,246 @@
-import React from 'react';
-import './Filter.scss';
-const FilterComponent = () => {
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import './Filter.scss'
+const FilterComponent = ({ filters, onFiltersChange }) => {
+  const handleSortChange = e => {
+    onFiltersChange({ ...filters, sortBy: e.target.value })
+  }
+
+const handleAirlineChange = e => {
+  const airline = e.target.value
+  const isChecked = e.target.checked
+
+  const newAirlines = isChecked ? [airline] : [] // Chỉ giữ lại checkbox được chọn
+
+  onFiltersChange({ ...filters, airlines: newAirlines })
+}
+
+
   return (
-    <div id="filter" style={{ display: 'block' }}>
-      <div id="sap-xep">
-        <table id="flight-sort" className="sap-xep-ket-qua" style={{ width: '100%' }}>
+    <div id='filter' style={{ display: 'block' }}>
+      <div id='sap-xep'>
+        <table
+          id='flight-sort'
+          className='sap-xep-ket-qua'
+          style={{ width: '100%' }}
+        >
           <tbody>
-            <tr className="titleSort">
-              <td colSpan="2">
+            <tr className='titleSort'>
+              <td colSpan='2'>
                 <span>Sắp xếp</span>
               </td>
             </tr>
           </tbody>
         </table>
-        <div className="borderSort">
-          <table className="sort" style={{ width: '100%' }}>
+        <div className='borderSort'>
+          <table className='sort' style={{ width: '100%' }}>
             <tbody>
               <tr>
                 <td>
-                  <input id="sort-1" type="radio" value="abay-suggest" name="groupSort" />
-                  <label id="lbl-sort-1" htmlFor="sort-1">Abay đề xuất</label>
+                  <input
+                    id='sort-1'
+                    type='radio'
+                    value='abay-suggest'
+                    name='groupSort'
+                    onChange={handleSortChange}
+                    checked={filters.sortBy === 'abay-suggest'}
+                  />
+                  <label id='lbl-sort-1' htmlFor='sort-1'>
+                    Abay đề xuất
+                  </label>
                 </td>
               </tr>
               <tr>
                 <td>
-                  <input id="sort1" type="radio" value="price" name="groupSort" />
-                  <label htmlFor="sort1">Giá (Thấp tới Cao)</label>
+                  <input
+                    id='sort1'
+                    type='radio'
+                    value='price'
+                    name='groupSort'
+                    onChange={handleSortChange}
+                    checked={filters.sortBy === 'price'}
+                  />
+                  <label htmlFor='sort1'>Giá (Thấp tới Cao)</label>
                 </td>
               </tr>
               <tr>
                 <td>
-                  <input id="sort2" type="radio" value="time" name="groupSort" />
-                  <label htmlFor="sort2">Thời gian khởi hành</label>
+                  <input
+                    id='sort2'
+                    type='radio'
+                    value='time'
+                    name='groupSort'
+                    onChange={handleSortChange}
+                    checked={filters.sortBy === 'time'}
+                  />
+                  <label htmlFor='sort2'>Thời gian khởi hành</label>
                 </td>
               </tr>
               <tr>
-                <td className="checked">
-                  <input id="sort3" type="radio" value="airline" name="groupSort" defaultChecked />
-                  <label id="lbl-sort-3" htmlFor="sort3">Hãng hàng không</label>
+                <td className='checked'>
+                  <input
+                    id='sort3'
+                    type='radio'
+                    value='airline'
+                    name='groupSort'
+                    defaultChecked
+                    onChange={handleSortChange}
+                    checked={filters.sortBy === 'airline'}
+                  />
+                  <label id='lbl-sort-3' htmlFor='sort3'>
+                    Hãng hàng không
+                  </label>
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
       </div>
-      <div id="cphSubColumn_ctl00_divDisplay2">
+      <div id='cphSubColumn_ctl00_divDisplay2'>
         <table style={{ width: '100%' }}>
           <tbody>
-            <tr className="titleSort">
-              <td colSpan="2">
+            <tr className='titleSort'>
+              <td colSpan='2'>
                 <span>Chế độ hiển thị</span>
               </td>
             </tr>
           </tbody>
         </table>
-        <div className="borderSort">
-          <table className="display-mode" style={{ width: '100%' }}>
+        <div className='borderSort'>
+          <table className='display-mode' style={{ width: '100%' }}>
             <tbody>
               <tr>
-                <td className="checked">
-                  <input type="radio" name="rblDisplayMode" value="base" id="priceBase" />
-                  <label htmlFor="priceBase">Giá cơ bản cho 1 người</label>
+                <td className='checked'>
+                  <input
+                    type='radio'
+                    name='rblDisplayMode'
+                    value='base'
+                    id='priceBase'
+                  />
+                  <label htmlFor='priceBase'>Giá cơ bản cho 1 người</label>
                 </td>
               </tr>
               <tr>
                 <td>
-                  <input type="radio" name="rblDisplayMode" value="with-bag" id="priceWithBag" />
-                  <label htmlFor="priceWithBag">Giá cơ bản + phí hành lý</label>
+                  <input
+                    type='radio'
+                    name='rblDisplayMode'
+                    value='with-bag'
+                    id='priceWithBag'
+                  />
+                  <label htmlFor='priceWithBag'>Giá cơ bản + phí hành lý</label>
                 </td>
               </tr>
               <tr>
                 <td>
-                  <input type="radio" name="rblDisplayMode" value="business" id="priceBusiness" />
-                  <label htmlFor="priceBusiness">Giá vé hạng thương gia</label>
+                  <input
+                    type='radio'
+                    name='rblDisplayMode'
+                    value='business'
+                    id='priceBusiness'
+                  />
+                  <label htmlFor='priceBusiness'>Giá vé hạng thương gia</label>
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
       </div>
-      <table id="loc-ket-qua" style={{ width: '100%' }}>
+      <table id='loc-ket-qua' style={{ width: '100%' }}>
         <tbody>
-          <tr className="titleSort">
-            <td colSpan="2">
+          <tr className='titleSort'>
+            <td colSpan='2'>
               <span>Chọn Lọc</span>
             </td>
           </tr>
           <tr>
-            <td colSpan="2">
-              <div className="borderSort">
-                <table className="theo-hang-bay" style={{ width: '100%' }}>
+            <td colSpan='2'>
+              <div className='borderSort'>
+                <table className='theo-hang-bay' style={{ width: '100%' }}>
                   <tbody>
-                    <tr className="sub-title">
+                    <tr className='sub-title'>
                       <td>Hãng hàng không</td>
                       <td>
-                        <a className="remove-filter" type="airline" href="#">
+                        <a
+                          className='remove-filter'
+                          type='airline'
+                          href='#'
+                          onClick={() =>
+                            onFiltersChange({ ...filters, airlines: [] })
+                          }
+                        >
                           Hiển thị tất cả
                         </a>
                       </td>
                     </tr>
                     <tr>
                       <td>
-                        <input type="checkbox" value="VN" name="rblAirline" id="rblAirline_VN" />
-                        <label htmlFor="rblAirline_VN">&nbsp;Vietnam Airlines</label>
+                        <input
+                          type='checkbox'
+                          value='VN'
+                          name='rblAirline'
+                          id='rblAirline_VN'
+                          onChange={handleAirlineChange}
+                          checked={filters.airlines.includes('VN')}
+                        />
+                        <label htmlFor='rblAirline_VN'>
+                          &nbsp;Vietnam Airlines
+                        </label>
                       </td>
                       <td>
-                        <img src="/smVN.gif" alt="Vietnam Airlines Logo" />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <input type="checkbox" value="QH" name="rblAirline" id="rblAirline_QH" />
-                        <label htmlFor="rblAirline_QH">&nbsp;BamBoo Airways</label>
-                      </td>
-                      <td>
-                        <img src="/smQH.gif" alt="BamBoo Airways Logo" />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <input type="checkbox" value="VJ" name="rblAirline" id="rblAirline_VJ" />
-                        <label htmlFor="rblAirline_VJ">&nbsp;VietjetAir</label>
-                      </td>
-                      <td>
-                        <img src="/smVJ.gif" alt="VietjetAir Logo" />
+                        <img src='/smVN.gif' alt='Vietnam Airlines Logo' />
                       </td>
                     </tr>
                     <tr>
                       <td>
-                        <input type="checkbox" value="VU" name="rblAirline" id="rblAirline_VU" />
-                        <label htmlFor="rblAirline_VU">&nbsp;Vietravel Airlines</label>
+                        <input
+                          type='checkbox'
+                          value='QH'
+                          name='rblAirline'
+                          id='rblAirline_QH'
+                          onChange={handleAirlineChange}
+                          checked={filters.airlines.includes('QH')}
+                        />
+                        <label htmlFor='rblAirline_QH'>
+                          &nbsp;BamBoo Airways
+                        </label>
                       </td>
                       <td>
-                        <img src="/smVU.gif" alt="Vietravel Airlines Logo" />
+                        <img src='/smQH.gif' alt='BamBoo Airways Logo' />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input
+                          type='checkbox'
+                          value='VJ'
+                          name='rblAirline'
+                          id='rblAirline_VJ'
+                          onChange={handleAirlineChange}
+                          checked={filters.airlines.includes('VJ')}
+                        />
+                        <label htmlFor='rblAirline_VJ'>&nbsp;VietjetAir</label>
+                      </td>
+                      <td>
+                        <img src='/smVJ.gif' alt='VietjetAir Logo' />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input
+                          type='checkbox'
+                          value='VU'
+                          name='rblAirline'
+                          id='rblAirline_VU'
+                          onChange={handleAirlineChange}
+                          checked={filters.airlines.includes('VU')}
+                        />
+                        <label htmlFor='rblAirline_VU'>
+                          &nbsp;Vietravel Airlines
+                        </label>
+                      </td>
+                      <td>
+                        <img src='/smVU.gif' alt='Vietravel Airlines Logo' />
                       </td>
                     </tr>
                   </tbody>
@@ -143,7 +251,7 @@ const FilterComponent = () => {
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default FilterComponent;
+export default FilterComponent
