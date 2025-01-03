057964fc9e2ae75c4fc1925bd7570d0d9a3d15f7
ducbinhnguyennd <ducbinhnguyennd@gmail.com>
2025-01-04 00:16:22 +0700

    update

--- src/router/router.js
+++ src/router/router.js
@@ -6,13 +6,17 @@ import GioiThieu from '../Layout/DeafaultLayout/GioiThieu/GioiThieu'
 import { VungLayout } from '../Layout/AdminLayout/VungLayout'
 import { SearchLayout } from '../Layout/SearchLayout'
 import { HangMayBayLayout } from '../Layout/AdminLayout/HangMayBayLayout'
+import DatVe from '../Layout/DatVe/DatVe'
+
 const publicRoutes = [
   { path: '/', component: TrangChuLayout },
   { path: '/lien-he', component: LienHe },
   { path: '/search', component: SearchLayout },
   { path: '/gioi-thieu-do-tho-y-yen', component: GioiThieu },
   { path: '/vung', component: VungLayout, layout: 'admin' },
-  { path: '/hangmaybay', component: HangMayBayLayout, layout: 'admin' }
+  { path: '/hangmaybay', component: HangMayBayLayout, layout: 'admin' },
+  { path: '/datve', component: DatVe },
+
 ]
 const privateRoutes = []
 export { publicRoutes, privateRoutes }
