4aa04eebba6a9766dac6e5d04b25933e633e1f55
trafdual <traz08102003@gmail.com>
2025-01-04 01:46:20 +0700

    f

--- src/components/SideBar/SearchSideBar.js
+++ src/components/SideBar/SearchSideBar.js
@@ -1,114 +1,227 @@
-import React from "react";
-import "./SearchSideBar.scss";
-function SearchSidebar() {
+import React, { useRef, useState, useEffect } from 'react'
+import './SearchSideBar.scss'
+import { useToast } from '../useToast/ToastContext'
+import { useNavigate } from 'react-router-dom'
+import TableTP from './TableTP'
+
+function SearchSidebar () {
+  const navigate = useNavigate()
+  const dropdownRef = useRef(null)
+  const { showToast, setSearchData, setcityto, setcityfrom } = useToast()
+  const [departure, setDeparture] = useState('Chọn điểm đi')
+  const [madepature, setmadepature] = useState('')
+  const [arrival, setArrival] = useState('Chọn điểm đến')
+  const [maarrival, setmaarrival] = useState('')
+  const [departureDate, setDepartureDate] = useState('')
+  const [returnDate, setReturnDate] = useState(null)
+  const [adults, setAdults] = useState(1)
+  const [children, setChildren] = useState(0)
+  const [infants, setInfants] = useState(0)
+
+  const [dropdownOpen, setDropdownOpen] = useState(null)
+
+  const formatDate = isoDate => {
+    const [year, month, day] = isoDate.split('-')
+    return `${day}/${month}/${year}`
+  }
+  const validate = () => {
+    let valid = true
+    if (madepature) {
+      valid = true
+    } else {
+      valid = false
+      showToast('Bạn chưa chọn điểm đi', 'warning')
+    }
+
+    if (maarrival) {
+      valid = true
+    } else {
+      valid = false
+      showToast('Bạn chưa chọn điểm đến', 'warning')
+    }
+    return valid
+  }
+
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(null)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  const handelSearch = async () => {
+    if (!validate()) return
+    try {
+      const requestData = {
+        departure: madepature,
+        arrival: maarrival,
+        date: formatDate(departureDate),
+        adults,
+        children,
+        infants
+      }
+
+      if (returnDate) {
+        requestData.returnDate = formatDate(returnDate)
+      }
+
+      const response = await fetch(
+        `https://wooordersystem.store/order-woo/api/getInfoFlights`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(requestData)
+        }
+      )
+
+      const data = await response.json()
+      if (response.ok) {
+        setSearchData(data)
+        navigate('/search')
+        window.location.reload()
+      }
+    } catch (error) {
+      console.error('Error fetching flight data:', error)
+    }
+  }
+
   return (
-    <div className="SearchSidebar">
+    <div className='SearchSidebar' ref={dropdownRef}>
       <table
-        width="100%"
-        cellPadding="0"
-        cellSpacing="0"
-        id="Table1"
-        className="sub-search-form"
+        width='100%'
+        cellPadding='0'
+        cellSpacing='0'
+        id='Table1'
+        className='sub-search-form'
       >
         <tbody>
           <tr>
-            <td className="sub-form-search">
-              <div className="borderSortlastItem">
-                <table width="100%">
+            <td className='sub-form-search'>
+              <div className='borderSortlastItem'>
+                <table width='100%'>
                   <tbody>
-                    <tr className="from">
-                      <td colSpan="6">
-                        <label htmlFor="departure">Điểm đi</label>
+                    <tr className='from'>
+                      <td colSpan='6'>
+                        <label htmlFor='departure'>Điểm đi</label>
                         <br />
                         <input
-                          name="departure"
-                          type="text"
-                          defaultValue="Tp Hồ Chí Minh"
-                          id="departure"
-                          className="departure startplace text-input ac_input focus-input"
-                          data-type="departure"
+                          name='departure'
+                          type='text'
+                          defaultValue='Tp Hồ Chí Minh'
+                          id='departure'
+                          className='departure startplace text-input ac_input focus-input'
+                          data-type='departure'
+                          value={departure}
+                          onClick={() => setDropdownOpen('departure')}
+                          onChange={e => setDeparture(e.target.value)}
+                          readOnly
                         />
-                        <input
-                          type="hidden"
-                          id="hdfstartplace"
-                          name="hdfstartplace"
-                          defaultValue="SGN"
-                        />
+                        {dropdownOpen === 'departure' && (
+                          <TableTP
+                            title='Chọn điểm đi'
+                            onSelect={(value, ma) => {
+                              setDeparture(value)
+                              setcityfrom(value)
+                              setmadepature(ma)
+                              setDropdownOpen(null)
+                            }}
+                          />
+                        )}
                       </td>
                     </tr>
 
                     <tr>
-                    <td>
-                    <span className="reverse-command">⥮</span>
-
-                    </td>
+                      <td>
+                        <span className='reverse-command'>⥮</span>
+                      </td>
                     </tr>
-                    <tr className="to">
-                      <td colSpan="6">
-                        <label htmlFor="arrival">Điểm đến</label>
+                    <tr className='to'>
+                      <td colSpan='6'>
+                        <label htmlFor='arrival'>Điểm đến</label>
                         <br />
                         <input
-                          name="arrival"
-                          type="text"
-                          defaultValue="Hà Nội"
-                          id="arrival"
-                          className="arrival endplace text-input ac_input focus-input"
-                          data-type="arrival"
+                          name='arrival'
+                          type='text'
+                          defaultValue='Hà Nội'
+                          id='arrival'
+                          className='arrival endplace text-input ac_input focus-input'
+                          data-type='arrival'
+                          value={arrival}
+                          onClick={() => setDropdownOpen('arrival')}
+                          onChange={e => setArrival(e.target.value)}
+                          readOnly
                         />
+                        {dropdownOpen === 'arrival' && (
+                          <TableTP
+                            title='Chọn điểm đến'
+                            onSelect={(value, ma) => {
+                              setArrival(value)
+                              setcityto(value)
+                              setmaarrival(ma)
+                              setDropdownOpen(null)
+                            }}
+                          />
+                        )}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className='departure' colSpan='3'>
+                        <label htmlFor='departureDate'>Ngày đi</label>
+                        <br />
                         <input
-                          type="hidden"
-                          id="hdfendplace"
-                          name="hdfendplace"
-                          defaultValue="HAN"
+                          name='departureDate'
+                          type='date'
+                          defaultValue='06/01/2025'
+                          id='departureDate'
+                          className='date-input txtDateLunar txtDateLunar-departure depDate focus-input'
+                          data-type='departure'
+                          value={departureDate}
+                          onChange={e => setDepartureDate(e.target.value)}
+                        />
+                      </td>
+                      <td className='return' colSpan='3'>
+                        <label htmlFor='returnDate'>Ngày về</label>
+                        <br />
+                        <input
+                          name='returnDate'
+                          type='date'
+                          id='returnDate'
+                          className='date-input txtDateLunar txtDateLunar-return retDate focus-input'
+                          data-type='return'
+                          value={returnDate}
+                          onChange={e => setReturnDate(e.target.value)}
                         />
                       </td>
                     </tr>
                     <tr>
-                      <td className="departure" colSpan="3">
-                        <label htmlFor="departureDate">Ngày đi</label>
-                        <br />
-                        <input
-                          name="departureDate"
-                          type="text"
-                          defaultValue="06/01/2025"
-                          id="departureDate"
-                          className="date-input txtDateLunar txtDateLunar-departure depDate focus-input"
-                          data-type="departure"
-                        />
-                      </td>
-                      <td className="return" colSpan="3">
-                        <label htmlFor="returnDate">Ngày về</label>
-                        <br />
-                        <input
-                          name="returnDate"
-                          type="text"
-                          id="returnDate"
-                          className="date-input txtDateLunar txtDateLunar-return retDate focus-input"
-                          data-type="return"
-                        />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td colSpan="13" style={{ paddingTop: "5px" }}>
+                      <td colSpan='13' style={{ paddingTop: '5px' }}>
                         <a
-                          href="/_WEB/PageSub/OtherPage/Xem-lich-am.aspx"
-                          className="lightview"
-                          rel="nofollow"
-                          style={{ fontSize: "12px", color: "orangered" }}
+                          href='/_WEB/PageSub/OtherPage/Xem-lich-am.aspx'
+                          className='lightview'
+                          rel='nofollow'
+                          style={{ fontSize: '12px', color: 'orangered' }}
                         >
                           * Xem lịch âm
                         </a>
-                        <span className="remove-date">Bỏ chọn ngày về</span>
+                        <span className='remove-date'>Bỏ chọn ngày về</span>
                       </td>
                     </tr>
-                    <tr className="passenger">
-                      <td colSpan="2">
-                        <label htmlFor="adults">Người lớn</label>
+                    <tr className='passenger'>
+                      <td colSpan='2'>
+                        <label htmlFor='adults'>Người lớn</label>
                         <br />
                         <select
-                          name="adults"
-                          id="adults"
-                          className="focus-input"
+                          name='adults'
+                          id='adults'
+                          className='focus-input'
+                          onChange={e => setAdults(e.target.value)}
+                          value={adults}
                         >
                           {[...Array(10)].map((_, i) => (
                             <option key={i} value={i}>
@@ -117,10 +230,15 @@ function SearchSidebar() {
                           ))}
                         </select>
                       </td>
-                      <td colSpan="2">
-                        <label htmlFor="children">Trẻ em</label>
+                      <td colSpan='2'>
+                        <label htmlFor='children'>Trẻ em</label>
                         <br />
-                        <select name="children" id="children">
+                        <select
+                          name='children'
+                          id='children'
+                          value={children}
+                          onChange={e => setChildren(Number(e.target.value))}
+                        >
                           {[...Array(10)].map((_, i) => (
                             <option key={i} value={i}>
                               {i}
@@ -128,10 +246,15 @@ function SearchSidebar() {
                           ))}
                         </select>
                       </td>
-                      <td colSpan="2">
-                        <label htmlFor="infants">Trẻ sơ sinh</label>
+                      <td colSpan='2'>
+                        <label htmlFor='infants'>Trẻ sơ sinh</label>
                         <br />
-                        <select name="infants" id="infants">
+                        <select
+                          name='infants'
+                          id='infants'
+                          value={infants}
+                          onChange={e => setInfants(Number(e.target.value))}
+                        >
                           {[...Array(7)].map((_, i) => (
                             <option key={i} value={i}>
                               {i}
@@ -141,16 +264,16 @@ function SearchSidebar() {
                       </td>
                     </tr>
                     <tr>
-                      <td colSpan="6"></td>
+                      <td colSpan='6'></td>
                     </tr>
                     <tr>
-                      <td colSpan="6"></td>
+                      <td colSpan='6'></td>
                     </tr>
-                    <tr align="center">
-                      <td colSpan="7" align="center" className="input-submit">
+                    <tr align='center'>
+                      <td colSpan='7' align='center' className='input-submit'>
                         <button
-                          className="button-text radius-5px btnSearchReCaptcha"
-                          onClick={() => alert("Đang xử lý...")}
+                          className='button-text radius-5px btnSearchReCaptcha'
+                          onClick={handelSearch}
                         >
                           Tìm Chuyến Bay
                         </button>
@@ -164,7 +287,7 @@ function SearchSidebar() {
         </tbody>
       </table>
     </div>
-  );
+  )
 }
 
-export default SearchSidebar;
+export default SearchSidebar
---
+++ src/components/SideBar/TableTP.js
@@ -0,0 +1,115 @@
+import { useState, useEffect } from 'react'
+import './TableTP.scss'
+
+const removeVietnameseTones = str => {
+  return str
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D')
+    .toLowerCase()
+}
+
+function TableTP ({ title, onSelect }) {
+  const [data, setdata] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [suggestions, setSuggestions] = useState([])
+
+  const fetchthanhpho = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/getfulltp')
+      const data = await response.json()
+      if (response.ok) {
+        setdata(data)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchthanhpho()
+  }, [])
+
+  useEffect(() => {
+    if (searchTerm.trim() === '') {
+      setSuggestions([])
+    } else {
+      const normalizedSearch = removeVietnameseTones(searchTerm)
+
+      const allSuggestions = data.flatMap(item =>
+        item.thanhpho.filter(
+          place =>
+            removeVietnameseTones(place.name).includes(normalizedSearch) ||
+            removeVietnameseTones(place.mathanhpho).includes(normalizedSearch)
+        )
+      )
+
+      setSuggestions(allSuggestions)
+    }
+  }, [searchTerm, data])
+
+  return (
+    <div className='destination-selector1'>
+      <h4>{title}</h4>
+      <div className='destination-list1'>
+        <div className='regions1'>
+          {data.map((item, index) => (
+            <div key={index} className='region1'>
+              <h3>{item.namevung}</h3>
+              <ul>
+                {item.thanhpho.map((place, i) => (
+                  <li
+                    key={i}
+                    className='destination-item1'
+                    onClick={() => onSelect(place.name, place.mathanhpho)}
+                  >
+                    {item.namevung === 'Việt Nam'
+                      ? `${place.name}`
+                      : `${place.name} (${place.mathanhpho})`}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+
+        <div className='divinputsearch1'>
+          <h5>
+            Nhập vào tên thành phố, hoặc tên sân bay, hoặc mã thành phố, hoặc mã
+            sân bay
+          </h5>
+          <input
+            type='text'
+            className='search-box1'
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+          <button>Chọn</button>
+        </div>
+
+        {suggestions.length > 0 && (
+          <div className='suggestion-box1'>
+            <ul>
+              {suggestions.map((place, index) => (
+                <li
+                  key={index}
+                  className='suggestion-item1'
+                  onClick={() => {
+                    onSelect(place.name, place.mathanhpho)
+                    setSearchTerm('')
+                    setSuggestions([])
+                  }}
+                >
+                  {`${place.name} (${place.mathanhpho})`}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default TableTP
