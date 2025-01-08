d22214fbbc1794059170e4a0bf359507a55e2244
trafdual <traz08102003@gmail.com>
2025-01-08 14:50:55 +0700

    first commit

--- src/Layout/SearchLayout/SearchQuocTe/SearchQuocTe.js
+++ src/Layout/SearchLayout/SearchQuocTe/SearchQuocTe.js
@@ -7,7 +7,9 @@ import FilterComponent from '../../../components/SideBar/Filter'
 import SearchSidebar from '../../../components/SideBar/SearchSideBar'
 import {
   LunarCalendarFormat,
-  formatDate
+  formatDate,
+  getSurroundingDates,
+  CalendarFormat
 } from '../../../components/LunarCalendarFormat/LunarCalendarFormat'
 import {
   applyFilters,
@@ -15,9 +17,6 @@ import {
 } from '../../../components/FilterChuyenBay/FilterChuyenBay'
 import {
   toggleDetails,
-  getAirlineImage,
-  getAirlineName,
-  handleDateClick,
   HandelTonggia,
   calculateDuration
 } from '../SearchLayoutFunction'
@@ -29,12 +28,13 @@ function SearchQuocTe () {
     mafrom,
     mato,
     date,
-    returnDate,
-    mangnguoi
+    mangnguoi,
+    setflightdata,
+    setSearchData,
+    setdate
   } = useToast()
-  const [activeDate, setActiveDate] = useState('Thứ Bảy')
+  const { previousTwoDays, nextTwoDays } = getSurroundingDates(date)
   const [visibleDetailIndex, setVisibleDetailIndex] = useState(null)
-  const [hangmaybay, sethangmaybay] = useState([])
   const [phantrams, setphantram] = useState([])
   const [selectedFlight, setSelectedFlight] = useState(null)
   const [filters, setFilters] = useState({
@@ -42,19 +42,6 @@ function SearchQuocTe () {
     airlines: []
   })
   const navigate = useNavigate()
-  console.log(searchData)
-
-  const fetchhang = async () => {
-    try {
-      const response = await fetch('https://demovemaybay.shop/gethangmaybay')
-      const data = await response.json()
-      if (response.ok) {
-        sethangmaybay(data)
-      }
-    } catch (error) {
-      console.error(error)
-    }
-  }
 
   const fetchphantram = async () => {
     try {
@@ -67,41 +54,61 @@ function SearchQuocTe () {
       console.error(error)
     }
   }
-  console.log(returnDate)
+
   useEffect(() => {
-    fetchhang()
     fetchphantram()
   }, [])
 
-  const dates = [
-    { day: 'Thứ Hai', date: '06/01', price: '868,000đ' },
-    { day: 'Thứ Ba', date: '06/01', price: '868,000đ' },
-    { day: 'Thứ Tư', date: '01/01', price: '' },
-    { day: 'Thứ Năm', date: '02/01', price: '' },
-    { day: 'Thứ Sáu', date: '03/01', price: '1,010,000đ' },
-    { day: 'Thứ Bảy', date: '04/01', price: '868,000đ' },
-    { day: 'Chủ Nhật', date: '05/01', price: '1,008,000đ' }
-  ]
+  const handleSearch = async date => {
+    try {
+      const requestData = {
+        departure: mafrom,
+        arrival: mato,
+        date: formatDate(date),
+        adults: mangnguoi[0]?.songuoi,
+        children: mangnguoi[1]?.songuoi || 0,
+        infants: mangnguoi[2]?.songuoi || 0
+      }
 
-  const flights1 = applyFilters(searchData.outBound.data.flights, filters)
+      const response = await fetch(
+        'https://wooordersystem.store/order-woo/api/getInfoFlightInternational',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(requestData)
+        }
+      )
+      const data = await response.json()
+
+      if (response.ok) {
+        setSearchData(data)
+        setdate(date)
+        window.location.reload()
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const flights1 = applyFilters(searchData.data, filters)
   const totalPeople = mangnguoi.reduce((total, item) => total + item.songuoi, 0)
 
   const totalPrice = mangnguoi.reduce((total, item) => {
     if (
       !selectedFlight ||
-      !selectedFlight.price ||
+      !selectedFlight.totalPrice ||
       !phantrams ||
       phantrams.length === 0
     ) {
       return total
     }
-    const pricePerTicket =
-      parseInt(selectedFlight.price.replace(/,/g, ''), 10) -
-      (parseInt(selectedFlight.price.replace(/,/g, ''), 10) *
+    const totalPricePerTicket =
+      parseInt(selectedFlight.totalPrice.replace(/,/g, ''), 10) -
+      (parseInt(selectedFlight.totalPrice.replace(/,/g, ''), 10) *
         phantrams[0].phantram) /
         100
-    const taxAndFee = (pricePerTicket * 30) / 100
-    return total + pricePerTicket * item.songuoi + taxAndFee * item.songuoi
+    const taxAndFee = (totalPricePerTicket * 30) / 100
+    return total + totalPricePerTicket * item.songuoi + taxAndFee * item.songuoi
   }, 0)
 
   return (
@@ -142,15 +149,33 @@ function SearchQuocTe () {
             </div>
 
             <div className='date-selection'>
-              {dates.map(({ day, date, price }) => (
+              {previousTwoDays.map((day, index) => {
+                const isPastDate =
+                  new Date(day) < new Date().setHours(0, 0, 0, 0)
+
+                return (
+                  <div
+                    key={index}
+                    className={`date ${isPastDate ? 'disabled' : ''}`}
+                    onClick={!isPastDate ? () => handleSearch(day) : undefined}
+                  >
+                    {CalendarFormat(day)}
+                    <br />
+                  </div>
+                )
+              })}
+              <div className={`date ${date ? 'active' : ''}`}>
+                {CalendarFormat(date)}
+                <br />
+              </div>
+              {nextTwoDays.map((day, index) => (
                 <div
-                  key={day}
-                  className={`date ${activeDate === day ? 'active' : ''}`}
-                  onClick={() => handleDateClick(day, setActiveDate)}
+                  key={index}
+                  className={`date`}
+                  onClick={() => handleSearch(day)}
                 >
-                  {day}
+                  {CalendarFormat(day)}
                   <br />
-                  {price || date}
                 </div>
               ))}
             </div>
@@ -193,8 +218,7 @@ function SearchQuocTe () {
             </div>
 
             <div className='flight-options-quocte'>
-              {(activeDate === 'Thứ Bảy' || activeDate === 'Thứ Hai') &&
-                Array.isArray(searchData.outBound.data.flights) &&
+              {Array.isArray(searchData.data) &&
                 flights1.map((flight, index) => (
                   <div
                     key={index}
@@ -202,17 +226,6 @@ function SearchQuocTe () {
                     style={{ cursor: 'pointer' }}
                   >
                     <div className='flight-row-quocte'>
-                      <div className='flight-info-quocte-logo'>
-                        <span>
-                          <img
-                            src={getAirlineImage(
-                              flight.airlineCode,
-                              hangmaybay
-                            )}
-                            alt=''
-                          />
-                        </span>
-                      </div>
                       <div className='flight-info-quocte'>
                         <span className='flight-code-quocte'>
                           {mafrom} - {mato}
@@ -220,7 +233,8 @@ function SearchQuocTe () {
                       </div>
                       <div className='flight-info-quocte'>
                         <span className='flight-time-chang-quocte'>
-                          {flight.departureTime} - {flight.arrivalTime}
+                          {flight.outbound.departureTime} -{' '}
+                          {flight.outbound.arrivalTime}
                         </span>
                       </div>
                       <div className='flight-info-quocte'>
@@ -235,8 +249,8 @@ function SearchQuocTe () {
                           Thời gian bay:
                           <div className='flight-time-quocte'>
                             {calculateDuration(
-                              flight.departureTime,
-                              flight.arrivalTime
+                              flight.outbound.departureTime,
+                              flight.outbound.arrivalTime
                             )}
                           </div>
                         </div>
@@ -250,7 +264,8 @@ function SearchQuocTe () {
                             flight,
                             setVisibleDetailIndex,
                             setSelectedFlight,
-                            visibleDetailIndex
+                            visibleDetailIndex,
+                            setflightdata
                           )
                         }}
                         style={{ display: 'flex' }}
@@ -269,10 +284,8 @@ function SearchQuocTe () {
                       <div className='flight-price-quocte'>
                         {phantrams.length > 0
                           ? (
-                              parseInt(flight.price.replace(/,/g, ''), 10) -
-                              (parseInt(flight.price.replace(/,/g, ''), 10) *
-                                phantrams[0].phantram) /
-                                100
+                              flight.totalPrice -
+                              (flight.totalPrice * phantrams[0].phantram) / 100
                             ).toLocaleString()
                           : 'Đang tải...'}
                       </div>
@@ -297,8 +310,10 @@ function SearchQuocTe () {
                                     </b>
                                   </p>
                                   <p>
-                                    <b>{selectedFlight.departureTime}</b>,
-                                    {formatDate(date)}
+                                    <b>
+                                      {selectedFlight.outbound.departureTime}
+                                    </b>
+                                    ,{formatDate(date)}
                                   </p>
                                   <p>{`${cityfrom}`}</p>
                                 </td>
@@ -312,8 +327,8 @@ function SearchQuocTe () {
                                 >
                                   <p style={{ paddingRight: '25px' }}>
                                     {calculateDuration(
-                                      selectedFlight.departureTime,
-                                      selectedFlight.arrivalTime
+                                      selectedFlight.outbound.departureTime,
+                                      selectedFlight.outbound.arrivalTime
                                     )}
                                   </p>
                                   <p>
@@ -338,47 +353,10 @@ function SearchQuocTe () {
                                     </b>
                                   </p>
                                   <p>
-                                    <b>{selectedFlight.arrivalTime}</b>,
-                                    {formatDate(date)}
+                                    <b>{selectedFlight.outbound.arrivalTime}</b>
+                                    ,{formatDate(date)}
                                   </p>
                                   <p>{`${cityto}`}</p>
-                                </td>
-                                <td style={{ width: '30%' }}>
-                                  <table
-                                    width='100%'
-                                    cellPadding='0'
-                                    cellSpacing='0'
-                                  >
-                                    <tbody>
-                                      <tr>
-                                        <td style={{ textAlign: 'left' }}>
-                                          <img
-                                            align='absmiddle'
-                                            src={getAirlineImage(
-                                              selectedFlight.airlineCode,
-                                              hangmaybay
-                                            )}
-                                            alt='Airline Logo'
-                                          />
-                                        </td>
-                                        <td
-                                          style={{
-                                            lineHeight: '18px',
-                                            padding: 0
-                                          }}
-                                        >
-                                          {getAirlineName(
-                                            selectedFlight.airlineCode,
-                                            hangmaybay
-                                          )}{' '}
-                                          <br />
-                                          Chuyến bay:
-                                          <b>{selectedFlight.flightNumber}</b>
-                                          <br />
-                                        </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
                                 </td>
                               </tr>
                             </tbody>
@@ -413,15 +391,8 @@ function SearchQuocTe () {
                                   </td>
                                   <td align='center' className='pax'>
                                     {(
-                                      parseInt(
-                                        selectedFlight.price.replace(/,/g, ''),
-                                        10
-                                      ) *
-                                        item.songuoi -
-                                      ((parseInt(
-                                        selectedFlight.price.replace(/,/g, ''),
-                                        10
-                                      ) *
+                                      selectedFlight.totalPrice * item.songuoi -
+                                      ((selectedFlight.totalPrice *
                                         phantrams[0].phantram) /
                                         100) *
                                         item.songuoi
@@ -429,18 +400,9 @@ function SearchQuocTe () {
                                   </td>
                                   <td align='center' className='pax'>
                                     {(
-                                      ((parseInt(
-                                        selectedFlight.price.replace(/,/g, ''),
-                                        10
-                                      ) *
+                                      ((selectedFlight.totalPrice *
                                         item.songuoi -
-                                        ((parseInt(
-                                          selectedFlight.price.replace(
-                                            /,/g,
-                                            ''
-                                          ),
-                                          10
-                                        ) *
+                                        ((selectedFlight.totalPrice *
                                           phantrams[0].phantram) /
                                           100) *
                                           item.songuoi) *
@@ -450,18 +412,8 @@ function SearchQuocTe () {
                                   </td>
                                   <td align='center' className='pax'>
                                     {HandelTonggia(
-                                      parseInt(
-                                        selectedFlight.price.replace(/,/g, ''),
-                                        10
-                                      ) *
-                                        item.songuoi -
-                                        ((parseInt(
-                                          selectedFlight.price.replace(
-                                            /,/g,
-                                            ''
-                                          ),
-                                          10
-                                        ) *
+                                      selectedFlight.totalPrice * item.songuoi -
+                                        ((selectedFlight.totalPrice *
                                           phantrams[0].phantram) /
                                           100) *
                                           item.songuoi,
